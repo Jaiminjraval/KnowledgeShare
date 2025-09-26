0c70f44504d7f41b890c31147ea7469abc7e0e46
Jay Varu <varujayj@gmail.com>
2025-09-26 23:03:00 +0530

    Added core models for Subject, User and SessionRequest

---
+++ KnowledgeShare/Models/Subject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace KnowledgeShare.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        // tutor - subject --> many-to-many
+        public virtual ICollection<ApplicationUser> Tutors { get; set; } = new List<ApplicationUser>();
+    }
+}
