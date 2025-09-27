9bfef5519a930821ac217198b61ea99d2c0637ad
Jay Varu <varujayj@gmail.com>
2025-09-27 18:53:10 +0530

    Configured ApplicationDbContext and added initial migration

--- KnowledgeShare/Data/ApplicationDbContext.cs
+++ KnowledgeShare/Data/ApplicationDbContext.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using KnowledgeShare.Models;
 
 namespace KnowledgeShare.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<SessionRequest> SessionRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>()
+                .HasMany(u => u.Subjects)
+                .WithMany(s => s.Tutors);
+
+            builder.Entity<SessionRequest>()
+                .HasOne(sr => sr.Learner)
+                .WithMany(u => u.SentSessionRequests)
+                .HasForeignKey(sr => sr.LearnerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<SessionRequest>()
+                .HasOne(sr => sr.Tutor)
+                .WithMany(u => u.ReceivedSessionRequests)
+                .HasForeignKey(sr => sr.TutorId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
